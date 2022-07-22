85e1d97ed21680e6391e7fd0757fb78125234c6d
Dhruv <dhruv.p@simformsolutions.com>
2022-07-22 11:20:40 +0530

    commit for testing

---
+++ src/main/java/com/example/jenkins/SpringJenkinsApplication.java
@@ -0,0 +1,26 @@
+package com.example.jenkins;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+	@PostConstruct
+	public void method(){
+		logger.info("Application started...");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed...");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
